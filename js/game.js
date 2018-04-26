c92e9ac92b92887dd050f580a07e7b9506d4b572
Tracy Williams <selpilot@gmail.com>
2018-04-26 09:49:57 -0700

    found bug. px where it shouldnt have been

--- js/game.js
+++ js/game.js
@@ -13,13 +13,13 @@ ctx.font = '30px Arial'; //Text size and Font
 ctx.fillStyle = '#00ff00'; //Text Color
 Bug.level = 1; //Holds current level in range [1-9]
 var MAX_LEVEL = 9;
-Bug.minCar = 2; //Min number of cars/row  
+Bug.minCar = 2; //Min number of cars/row
 Bug.filenames = ['assets/green-52.png', //binary-80px
-'assets/green-109px.png', //binary-120px
-'assets/green-167.png', //binary-160px
-'assets/green-195.png', //binary-200px
-'assets/green-225.png', //binary-240px
-'assets/green-280.png']; //binary-280px
+  'assets/green-109.png', //binary-120px
+  'assets/green-167.png', //binary-160px
+  'assets/green-195.png', //binary-200px
+  'assets/green-225.png', //binary-240px
+  'assets/green-280.png']; //binary-280px
 Bug.maxSeparation = [8, 7, 6, 5, 5, 4, 4, 4, 4, 4];
 Bug.minSeparation = [6, 5, 5, 4, 3, 3, 3, 2, 2, 2];
 Bug.maxObsLength = [4, 4, 4, 5, 5, 5, 6, 6, 6, 6];
